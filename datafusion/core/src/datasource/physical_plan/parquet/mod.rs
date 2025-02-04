3a498b48e1695d952bf48d5d59186f80ae2070fb
cetra3 <cetra3@hotmail.com>
2025-02-04 15:46:27 +1030

    Make parquet pushdown filters public

--- datafusion/core/src/datasource/physical_plan/parquet/mod.rs
+++ datafusion/core/src/datasource/physical_plan/parquet/mod.rs
@@ -23,10 +23,7 @@ use std::sync::Arc;
 
 use crate::datasource::listing::PartitionedFile;
 use crate::datasource::physical_plan::file_stream::FileStream;
-use crate::datasource::physical_plan::{
-    parquet::page_filter::PagePruningAccessPlanFilter, DisplayAs, FileGroupPartitioner,
-    FileScanConfig,
-};
+use crate::datasource::physical_plan::{DisplayAs, FileGroupPartitioner, FileScanConfig};
 use crate::{
     config::{ConfigOptions, TableParquetOptions},
     error::Result,
@@ -62,8 +59,10 @@ use crate::datasource::schema_adapter::{
 pub use access_plan::{ParquetAccessPlan, RowGroupAccess};
 pub use metrics::ParquetFileMetrics;
 use opener::ParquetOpener;
+pub use page_filter::PagePruningAccessPlanFilter;
 pub use reader::{DefaultParquetFileReaderFactory, ParquetFileReaderFactory};
 pub use row_filter::can_expr_be_pushed_down_with_schemas;
+pub use row_group_filter::RowGroupAccessPlanFilter;
 pub use writer::plan_to_parquet;
 
 /// Execution plan for reading one or more Parquet files.
